247775a8deda2d4ad2fbe936e6c870b9ad403790
yangxiushan <501942821@qq.com>
2020-10-21 09:29:19 +0800

    echo

--- ls_me.c
+++ ls_me.c
@@ -1,25 +1,35 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+int
+main(void)
 {
-    DIR    *dp;
-    struct dirent *dirp;
+    char buf[MAXLINE];
+    pid_t pid;
+    int status;
 
-    if (argc != 2) {
-        printf("usage: ls directory_name");
-        exit(1);
-    }
+    printf("%% ");
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if (buf[strlen(buf)-1] == '\n')
+            buf[strlen(buf)-1] = 0;
 
-    if ((dp = opendir(argv[1])) == NULL) {
-        printf("can't open %s", argv[1]);
-        exit(1);
+        if ((pid = fork()) < 0)
+            exit(1);
+        else if (pid == 0) {
+            execlp(buf, buf, (char *)0);
+            printf("couldn't execute: %s\n", buf);
+            exit(127);
+        }
+
+        if ((pid = waitpid(pid, &status, 0)) < 0) {
+            printf("waitpid error");
+            exit(1);
+        }
+        printf("%% ");
     }
-    while ((dirp = readdir(dp)) != NULL)
-    {
-        printf("%s\n", dirp->d_name);
-    }
-    closedir(dp);
     exit(0);
 }
